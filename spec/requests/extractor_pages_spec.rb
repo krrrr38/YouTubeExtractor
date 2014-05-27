82df3cd0ba922e19447522b2b7693dade5993f93
krrrr38 <k.kaizu38@gmail.com>
2014-05-28 08:24:51 +0900

    Add social buttons

--- spec/requests/extractor_pages_spec.rb
+++ spec/requests/extractor_pages_spec.rb
@@ -6,7 +6,7 @@ describe "ExtractorPages" do
   describe "Home page" do
     before { visit root_path }
 
-    it { should have_content('YoutubeExtractor') }
+    it { should have_content('YouTube Extractor') }
     it { should have_title(full_title('')) }
     it { should_not have_title('| Home') }
   end
--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -1,5 +1,5 @@
 def full_title(page_title)
-  base_title = "YoutubeExtractor"
+  base_title = "YouTube Extractor"
   if page_title.empty?
     base_title
   else
